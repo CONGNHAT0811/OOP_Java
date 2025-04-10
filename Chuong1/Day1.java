0ffb48e0da536809d3a5800bb5839afb08e21187
Công Nhất <congnhatle36@gmail.com>
2025-04-10 10:26:25 +0700

    Bài tập Day 1

---
+++ Chuong1/Day1.java
@@ -0,0 +1,24 @@
+package Chuong1;
+
+public class Day1 {
+    public static void main(String[] args) {
+        // In thông tin cá nhân
+        System.out.println("Mã Sinh Viên: 22210503332221050333");
+        System.out.println("Họ & Tên: Lê Công Nhất");
+        System.out.println("Khoa : Công Nghệ Thông Tin");
+        System.out.println("Ngành: Khoa Học Dữ Liệu");
+        System.out.println("Khóa: 67");
+        System.out.println("Lớp: DCCTKH67A");
+        System.out.println("Học Phần: Lập Trình Hướng Đối Tượng Với Java");
+        int namsinh = 2003;
+        int namHienTai = 2025;
+        int tuoi = namHienTai - namsinh;
+        System.out.println("Tuổi: " + tuoi);
+        int a = 5;
+        int b = 10;
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a - b = " + (a - b));
+        System.out.println("a * b = " + (a * b));
+        System.out.println("a / b = " + (a / b));
+    }
+}
---
+++ Chuong1/Readme_Chương1.md
@@ -0,0 +1,7 @@
+Mã Sinh Viên: 22210503332221050333
+Họ & Tên: Lê Công Nhất
+Khoa : Công Nghệ Thông Tin
+Ngành: Khoa Học Dữ Liệu
+Khóa: 67
+Lớp: DCCTKH67ADCCTKH67A
+Học Phần: Lập Trình Hướng Đối Tượng Với Java
